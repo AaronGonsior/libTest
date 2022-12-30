c7ac12c99014e2b04697a29c3e9940a9edda8a93
AaronGonsior <aaron.gonsior@gmail.com>
2022-12-30 18:13:13 +0100

    fix

---
+++ libTest-demo/demo.go
@@ -0,0 +1,10 @@
+package main
+
+import(
+"fmt"
+lib "github.com/AaronGonsior/libTest"
+)
+
+func main() {
+fmt.Println(lib.Test())
+}
---
+++ libTest-demo/go.mod
@@ -0,0 +1,3 @@
+module github.com/AaronGonsior/libTest@v0.1.0
+
+go 1.16
